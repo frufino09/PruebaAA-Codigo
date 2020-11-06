4de1ebe9da3a37587570caba74084c45ab8da6df
Frank <fruifino.09@gmail.com>
2020-11-06 18:46:21 -0500

    clear code

--- PruebaAA/Models/DBContext.cs
+++ PruebaAA/Models/DBContext.cs
@@ -19,7 +19,6 @@ namespace PruebaAA.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=DESKTOP-5PUB6O5;Database=Prueba;Trusted_Connection=True;");
             }
         }
