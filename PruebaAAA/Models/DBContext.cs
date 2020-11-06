585c64481418899b20ea8891e6abae370bda8a87
frufino09 <41161930+frufino09@users.noreply.github.com>
2020-11-06 17:18:18 -0500

    Add files via upload
    Fist Commit

---
+++ PruebaAAA/Models/DBContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PruebaAAA.Models
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Inventory> Inventory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-5PUB6O5;Database=Prueba;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>(entity =>
+            {
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.PointOfSale)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.Product)
+                    .IsRequired()
+                    .HasMaxLength(150);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PruebaAAA/Services/InventoryService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using PruebaAAA.Collections;
+using PruebaAAA.Models;
+
+namespace PruebaAAA.Services
+{
+    public static class InventoryService
+    {
+        /// <summary>
+        /// Read and transform the data in the inventory objects list from a csv file
+        /// </summary>
+        /// <param name="url">Url path of the csv file</param>
+        public static async Task<IEnumerable<Inventory>> ReadAndTransformFileAsync(string url)
+        {
+            var resultInventoryList = new List<Inventory>();
+
+            var request = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+            var response = await request.GetResponseAsync();
+            using var streamReader = new StreamReader(response.GetResponseStream() ?? throw new InvalidOperationException());
+            while (!streamReader.EndOfStream)
+            {
+                var line = await streamReader.ReadLineAsync();
+
+                if(line == null || string.IsNullOrWhiteSpace(line) || line.Equals("PointOfSale;Product;Date;Stock"))
+                    continue;
+
+                var lineParse = line.Split(';');
+                resultInventoryList.Add(new Inventory
+                {
+                    PointOfSale = lineParse[0],
+                    Product = lineParse[1],
+                    Date = DateTime.ParseExact(lineParse[2], "yyyy-MM-dd",  CultureInfo.InvariantCulture),
+                    Stock = int.Parse(lineParse[3])
+                });
+            }
+
+            return resultInventoryList;
+        }
+
+        /// <summary>
+        /// Remove exiting data in the database.
+        /// </summary>
+        public static async Task RemoveAsync()
+        {
+            await InventoryCollection.RemoveAsync();
+        }
+
+        /// <summary>
+        /// Save the data in the database.
+        /// </summary>
+        /// <param name="inventoryList">List of Inventory object.</param>
+        public static async Task SaveAsync(IEnumerable<Inventory> inventoryList)
+        {
+            await InventoryCollection.SaveAsync(inventoryList);
+        }
+    }
+}
